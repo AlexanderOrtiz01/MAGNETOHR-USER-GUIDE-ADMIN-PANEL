fe770534f14e28a0e4190bc67f5aae24cf24a721
AlexanderOrtiz01 <alexvillalta0108@gmail.com>
2026-02-05 19:37:41 -0600

    Cambiar ruta de guides-company a guides-admin-panel y actualizar enlace del botón para ir directamente a KPIs

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,34 +40,34 @@ export default defineConfig({
 				{
 					label: 'Panel General (Overview Dashboard)',
 					items: [
-						{ label: 'Conociendo los KPIs', slug: 'guides-company/getting-to-know-kpis' },
-						{ label: 'Conociendo las Tablas (Tables)', slug: 'guides-company/getting-to-know-tables' },
+					{ label: 'Conociendo los KPIs', slug: 'guides-admin-panel/getting-to-know-kpis' },
+					{ label: 'Conociendo las Tablas (Tables)', slug: 'guides-admin-panel/getting-to-know-tables' },
 					],
 				},
 				{
 					label: 'Soporte Y Moderación (Support & Moderation)',
 					items: [
-						{ label: 'Ofertas de Trabajo Pendientes (Pending Job Posts)', slug: 'guides-company/pending-job-posts' },
-						{ label: 'Ofertas de Trabajo Aprobadas (Approved Job Posts)', slug: 'guides-company/approved-job-posts' },
-						{ label: 'Ofertas de Trabajo Rechazadas (Rejected Job Posts)', slug: 'guides-company/rejected-job-posts' },
+					{ label: 'Ofertas de Trabajo Pendientes (Pending Job Posts)', slug: 'guides-admin-panel/pending-job-posts' },
+					{ label: 'Ofertas de Trabajo Aprobadas (Approved Job Posts)', slug: 'guides-admin-panel/approved-job-posts' },
+					{ label: 'Ofertas de Trabajo Rechazadas (Rejected Job Posts)', slug: 'guides-admin-panel/rejected-job-posts' },
 					],
 				},
 				{
 					label: 'Descuentos En Funciones Premium (Premium Features Discount)',
 					items: [
-						{ label: '¿Cómo le asigno un descuento a una compañía o candidato?', slug: 'guides-company/assign-discount' },
+						{ label: '¿Cómo le asigno un descuento a una compañía o candidato?', slug: 'guides-admin-panel/assign-discount' },
 					],
 				},
 				{
 					label: 'Buscador De Job Seekers (Job Seeker Finder)',
 					items: [
-						{ label: '¿Cómo busco un candidato?', slug: 'guides-company/search-candidate' },
+						{ label: '¿Cómo busco un candidato?', slug: 'guides-admin-panel/search-candidate' },
 					],
 				},
 				{
 					label: 'Usuarios (Users)',
 					items: [
-						{ label: '¿Cómo veo los Usuarios Administradores?', slug: 'guides-company/view-admin-users' },
+						{ label: '¿Cómo veo los Usuarios Administradores?', slug: 'guides-admin-panel/view-admin-users' },
 					],
 				},
 			],
